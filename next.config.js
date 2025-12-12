c6c359e55b65189816c6d9e923576c44cb41036d
Bexruz <bexruzbek263@gmail.com>
2025-12-13 02:19:01 +0500

    vercel fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  // Ensure Prisma Client is properly bundled
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 }
 
 module.exports = nextConfig
